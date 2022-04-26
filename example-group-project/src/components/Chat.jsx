48954249db6b602868229a3fe6feee68b5e83cc3
Christos Nicolaou <chrisanicolaou>
2022-04-26 10:25:58 +0100

    Adds components

---
+++ example-group-project/src/components/Chat.jsx
@@ -0,0 +1,13 @@
+const Chat = () => {
+  //LOGIC FOR GETTING AND DISPLAYING CHAT FROM API
+
+  return (
+    <div>
+      <ul>
+        <li>This is where the chat would go</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ example-group-project/src/components/SendingMessage.jsx
@@ -0,0 +1,11 @@
+const SendingMessage = () => {
+  //LOGIC FOR OPTIMISTICALLY RENDERING SENT MESSAGE, & POSTING TO API
+  return (
+    <form>
+      <input></input>
+      <button>Send</button>
+    </form>
+  );
+};
+
+export default SendingMessage;
