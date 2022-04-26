483e6973ff8bea41e0e2a244c2ff6791dfc20fec
Christos Nicolaou <chrisanicolaou>
2022-04-26 13:35:05 +0100

    Fixes post-req-logic

--- example-group-project/src/components/Chat.jsx
+++ example-group-project/src/components/Chat.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
-import { getReq } from "../utils/api";
+import { useState, useEffect } from "react";
+import { postReq, getReq } from "../utils/api";
 
 const Chat = () => {
   const [commentsList, setCommentsList] = useState([]);
+  const [newMessageToPost, setNewMessageToPost] = useState("");
 
   useEffect(() => {
     getReq(`/api/messages`).then((res) => {
@@ -11,10 +12,35 @@ const Chat = () => {
     });
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let dataToPost = { username: "developer_f", body: newMessageToPost };
+    setCommentsList((currCommentsList) => {
+      return [
+        ...currCommentsList,
+        {
+          username: "developer_f",
+          body: newMessageToPost,
+          comment_id: `new_${(Math.random() * 100).toString()}`,
+        },
+      ];
+    });
+    postReq(`/api/messages`, dataToPost)
+      .then((res) => {
+        return getReq(`/api/messages`);
+      })
+      .then((res) => {
+        setCommentsList(res);
+      });
+    setNewMessageToPost("");
+  };
+
   return (
     <div>
       <ul>
-        {commentsList.map((comment) => {
+        {commentsList.map((comment, index) => {
+          if (index === 0) console.log("new request");
+          console.log(comment.comment_id);
           return (
             <li key={comment.comment_id}>
               <p>{comment.body}</p>
@@ -23,6 +49,13 @@ const Chat = () => {
           );
         })}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={newMessageToPost}
+          onChange={(e) => setNewMessageToPost(e.target.value)}
+        ></input>
+        <button>Send</button>
+      </form>
     </div>
   );
 };
